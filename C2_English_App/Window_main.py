196dbad9ac0c896cfb9e0238724f51eb1fe93565
PSzczubiala <biuro@excellentenglish.pl>
2026-02-19 10:11:50 +0100

    Submit: project files

---
+++ C2_English_App/Window_main.py
@@ -0,0 +1,27 @@
+import customtkinter as ctk
+from PIL import Image
+
+from Window_menu import run_window_menu
+from class_AppButton import AppButton
+
+
+# wymiary głównego okna oraz przesunięcie od krawędzi ekranu
+main_page = ctk.CTk(fg_color="white")
+main_page.title("The ultimate guide to C2 Proficiency level!")
+main_page.geometry('400x300+600+250')
+main_page.resizable(False,False)
+
+# Photo for main page
+# 1. Wczytanie obrazu z pliku.
+image = Image.open("Photo for title page.png")
+# 2. Utworzenie obiektu CTkImage.
+photo_for_main_page = ctk.CTkImage(light_image=image, dark_image=image, size=(280,140))
+# 3. Utworzenie Labela, który TEN obraz wyświetla.
+label = ctk.CTkLabel(main_page, image=photo_for_main_page, text="")
+label.place(x=65, y=30)
+
+# przycisk "Let's get started!" wraz z parametrami i ułożeniem na ekranie
+button_start_app= AppButton(main_page, text="Let's get started!", command=lambda: run_window_menu(main_page))
+button_start_app.place(x=120, y=200)
+
+main_page.mainloop()
--- Sokker GUI/test_database.py
+++
@@ -1,4 +0,0 @@
-from class_Database_actions import WordDatabase
-
-def main():
-    my_database = WordDatabase.create_database_vocabulary()
