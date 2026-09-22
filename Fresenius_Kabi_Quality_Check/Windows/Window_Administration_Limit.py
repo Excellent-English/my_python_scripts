4e1761126bbffcb7cea4713f60a0fae50256e375
PSzczubiala <biuro@excellentenglish.pl>
2026-09-22 14:48:50 +0200

    New comment while commiting scripts

--- Fresenius_Kabi_Quality_Check/Windows/Window_Administration_Limit.py
+++ Fresenius_Kabi_Quality_Check/Windows/Window_Administration_Limit.py
@@ -6,16 +6,15 @@ from Fresenius_Kabi_Quality_Check.AllClasses.Button_Standard import Button_Stand
 from Fresenius_Kabi_Quality_Check.AllClasses.App_Window import AppWindow
 from Fresenius_Kabi_Quality_Check.AllClasses.App_Frame import AppFrame
 from Fresenius_Kabi_Quality_Check.AllClasses.App_Label_Title import App_Label_Title
-from Fresenius_Kabi_Quality_Check.Windows.Window_Administration_Limit import run_window_administraton_limit
 
 
-def run_window_administraton(menu_page):
+def run_window_administraton_limit(adm_page):
     # Zamknij / ukryj główne okno
-    menu_page.withdraw()   # albo destroy()
+    adm_page.withdraw()   # albo destroy()
     # ctk.deactivate_automatic_dpi_awareness()
 
     # Utwórz nowe okno menu
-    adm_page = AppWindow(banner_text = "Administration", width=1100, height=600, x= 110, y = 30, fg_color="#F6F7F9")
+    limit_page = AppWindow(banner_text = "Limit Management", width=1100, height=600, x= 110, y = 30, fg_color="#F6F7F9")
     # Gdyby była potrzeba zmiany tytułu w kolejnych oknach:
     # menu_page = AppWindow(title="Inny tytuł okna")
 
@@ -27,29 +26,29 @@ def run_window_administraton(menu_page):
     power_off_icon = ctk.CTkImage(light_image=image, dark_image=image, size=(35, 35))
     # 3. Przycisk z ikoną (bez tekstu) osadzony na banerze
     power_btn = ctk.CTkButton(
-        adm_page.banner_frame,
+        limit_page.banner_frame,
         image=power_off_icon,
         text="",
         width=35, height=35,
         fg_color="transparent",
         hover_color="#755a44",
-        command= lambda: adm_page.close_the_app(main_page)
+        command= lambda: limit_page.close_the_app(adm_page)
     )
     power_btn.image = power_off_icon  # trzymaj referencję!
     power_btn.place(x=900, y=5)
-    adm_page.bind("<Escape>", lambda event: adm_page.close_the_app(main_page))
+    limit_page.bind("<Escape>", lambda event: limit_page.close_the_app(adm_page))
 
-    logout_subtitle = ctk.CTkLabel(adm_page, text="Logout", font= ("Open Sans", 14), text_color = "white", fg_color = "#755a44")
+    logout_subtitle = ctk.CTkLabel(limit_page, text="Logout", font= ("Open Sans", 14), text_color = "white", fg_color = "#755a44")
     logout_subtitle.place(x=955, y=12)
 
 # ---------------------------------------------------------------------------------
 
     # Dodanie przycisku zawierającego ikonę return- przycisk powracający do poprzedniego okna
     def return_to_previous_window():
-        adm_page.withdraw()
-        menu_page.deiconify()
-        menu_page.lift()
-        menu_page.focus_force()
+        limit_page.withdraw()
+        adm_page.deiconify()
+        adm_page.lift()
+        adm_page.focus_force()
 
     # 1. Wczytanie obrazu z pliku
     image = Image.open("../Images/Return_icon.png")
@@ -57,7 +56,7 @@ def run_window_administraton(menu_page):
     power_off_icon = ctk.CTkImage(light_image=image, dark_image=image, size=(20,30))
     # 3. Przycisk z ikoną (bez tekstu) osadzony na banerze
     power_btn = ctk.CTkButton(
-        adm_page.banner_frame,
+        limit_page.banner_frame,
         image=power_off_icon,
         text="",
         width=20, height=30,
@@ -73,10 +72,10 @@ def run_window_administraton(menu_page):
     # zablokuj zamknięcie okna za pomocą "X"
     def disable_close():
         pass
-    adm_page.protocol("WM_DELETE_WINDOW", disable_close)
+    limit_page.protocol("WM_DELETE_WINDOW", disable_close)
 
 
-    line_bottom = ctk.CTkFrame(adm_page, height=2, width=1100, fg_color="#DDE2E7", corner_radius=0)
+    line_bottom = ctk.CTkFrame(limit_page, height=2, width=1100, fg_color="#DDE2E7", corner_radius=0)
     line_bottom.place(x=0, y=500)
 
 
@@ -84,17 +83,17 @@ def run_window_administraton(menu_page):
 # ramki i podpisy do ramek na głównej stronie
 # ---------------------------------------------------------------------------------------------------------
 
-    label_top_title = App_Label_Title(adm_page, text="Administration", font= ("Open Sans", 24, "bold"), text_color = "#755a44", fg_color="#F6F7F9")
+    label_top_title = App_Label_Title(limit_page, text="Administration", font= ("Open Sans", 24, "bold"), text_color = "#755a44", fg_color="#F6F7F9")
     label_top_title.place(x=450, y=90)
 
-    label_top_subtitle = App_Label_Title(adm_page, text="Manage users, limits and reporting settings for the system", font= ("Open Sans", 14), text_color = "#8B7A6B", fg_color="#F6F7F9")
+    label_top_subtitle = App_Label_Title(limit_page, text="Manage users, limits and reporting settings for the system", font= ("Open Sans", 14), text_color = "#8B7A6B", fg_color="#F6F7F9")
     label_top_subtitle.place(x=350, y=120)
 
 # ---------------------------------------------------------------------------------------------------------
 # ramki i podpisy do ramek na głównej stronie
 # ---------------------------------------------------------------------------------------------------------
 
-    frame_quality_check = AppFrame(adm_page, height= 250)
+    frame_quality_check = AppFrame(limit_page, height= 250)
     frame_quality_check.place(x=80, y=170)
 
     label_quality_check_title = App_Label_Title(frame_quality_check, text="People Management", font= ("Open Sans", 22, "bold"), text_color = "#755a44")
@@ -110,23 +109,18 @@ def run_window_administraton(menu_page):
 
 # ---------------------------------------------------------------------------------------------------------
 
-    frame_proposal = AppFrame(adm_page, height= 250)
+    frame_proposal = AppFrame(limit_page, height= 250)
     frame_proposal.place(x=395, y=170)
 
     label_proposal_title = App_Label_Title(frame_proposal, text="Limit management", font= ("Open Sans", 22, "bold"), text_color = "#755a44")
     label_proposal_title.place(x=50, y=130)
-
-    label_proposal_title.configure(cursor="hand2")
-    label_proposal_title.bind(
-        "<Button-1>",
-        lambda event: run_window_administraton_limit(adm_page))
 
     label_proposal_subtitle = App_Label_Title(frame_proposal, text="View and configure\nall tresholds and limits", font= ("Open Sans", 14), text_color = "#8B7A6B", justify="center")
     label_proposal_subtitle.place(x=75, y=180)
 
 # ---------------------------------------------------------------------------------------------------------
 
-    frame_administration = AppFrame(adm_page, height= 250)
+    frame_administration = AppFrame(limit_page, height= 250)
     frame_administration.place(x=710, y=170)
 
     label_administration_title = App_Label_Title(frame_administration, text="Reporting", font= ("Open Sans", 22, "bold"), text_color = "#755a44")
@@ -199,18 +193,11 @@ def run_window_administraton(menu_page):
     administration_btn.place(x=90, y=15)
 
 
-
-
-
-
-
-    # Zaprezentuj okno na ekranie komputera
-    adm_page.mainloop()
-
-# funkcja do uruchomienia okna dla testów, później do usunięcia
+    # funkcja do uruchomienia okna dla testów, później do usunięcia
 
 if __name__ == "__main__":
-    main_page = ctk.CTk()
-    main_page.withdraw()
+    adm_page = ctk.CTk()
+    adm_page.withdraw()
 
-    run_window_administraton(main_page)
+    run_window_administraton_limit(adm_page)
+    adm_page.mainloop()
